1f0dac718ea795c31cb3218fb1a5bb3f67c380b5
mqschwanda <mqschwanda@gmail.com>
2019-10-15 20:46:34 -0400

    add env variables

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
-  expect(1).toEqual(2);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
